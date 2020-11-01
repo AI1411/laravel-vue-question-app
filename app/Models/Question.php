2fbc43e639ae2a3ba35abf0e64a5bc2c94e6ef27
AI1411 <goethe0719@gmail.com>
2020-11-01 19:11:50 +0900

    Answerクラス実装

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -36,7 +36,7 @@ class Question extends Model
      */
     public function getStatusAttribute(): string
     {
-        if ($this->answers > 0) {
+        if ($this->answers_count > 0) {
             if ($this->best_answer_id) {
                 return "answered-accepted";
             }
@@ -50,5 +50,10 @@ class Question extends Model
         $parseDown = new \Parsedown();
 
         return $parseDown->text($this->body);
+    }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
     }
 }
